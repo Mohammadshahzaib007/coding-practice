6dfb4f690d369e080d06dcf835d2159c583ba0d0
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-01-16 23:30:47 +0530

    insertion sort

--- sorting.js
+++ sorting.js
@@ -36,4 +36,18 @@ function selectionSort(list) {
   return list;
 }
 
-console.log(selectionSort([2, 1, 12, 7, 6, 0, 32, 100, 90, 45, 23]));
+// console.log(selectionSort([2, 1, 12, 7, 6, 0, 32, 100, 90, 45, 23]));
+
+// Insertion sort
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currVal;
+  }
+  return arr;
+}
+
+console.log(insertionSort([2, 1, 9, 76, 4]));
