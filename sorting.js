9c91fcbcfcc31355a728d6bf2e61021a69273708
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-01-22 23:23:46 +0530

    written helper method for radix sort

--- sorting.js
+++ sorting.js
@@ -113,4 +113,24 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
   return arr;
 }
 
-console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+// console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+
+// Radix sort
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigit(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+console.log(mostDigit([2345, 3, 98733446]));
