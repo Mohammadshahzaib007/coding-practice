135a3f1594f07522e899059581add95e87a5635b
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-01-29 21:53:48 +0530

    Added pop

--- DSA/singly-linked-list.js
+++ DSA/singly-linked-list.js
@@ -20,22 +20,46 @@ class SinglyLinkedList {
   push(val) {
     // This function should accept a value
     // Create a new node using the value passed to the function
+    // If there is no head property on the list, set the head and tail to be the newly created node
+    // Otherwise set the next property on the tail to be the new node node
+    // and set the tail property on the list to be the newly created node
+    // Increment the length by one
     const newNode = new Node(val);
 
-    // If there is no head property on the list, set the head and tail to be the newly created node
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
     } else {
-      // Otherwise set the next property on the tail to be the new node node
-      // and set the tail property on the list to be the newly created node
       this.tail.next = newNode;
       this.tail = newNode;
     }
-    // Increment the length by one
     this.length++;
 
     return this;
+  }
+
+  pop() {
+    // if there are no nodes in the list, return undefined.
+    // Loop through the list until you reach the tail.
+    // Set the next property of the 2nd to last node to be null
+    // Set the tail to be the 2nd to last node
+    // Decrement the length of the list by 1
+    // Return the value of the node removed
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = 0;
+      this.tail = 0;
+    }
+    return current;
   }
 }
 
@@ -44,4 +68,4 @@ const list = new SinglyLinkedList();
 list.push("mohammad");
 list.push("shahzaib");
 list.push("coder");
-console.log(list);
+console.log(list.pop());
