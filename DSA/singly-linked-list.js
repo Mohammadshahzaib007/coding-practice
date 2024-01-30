b415db232ecaf795554b28203f3d2cf9a638792b
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-01-30 22:21:58 +0530

    Added unshift

--- DSA/singly-linked-list.js
+++ DSA/singly-linked-list.js
@@ -78,6 +78,29 @@ class SinglyLinkedList {
     }
     return temp;
   }
+
+  unshift(val) {
+    // - This function should accept a value
+    // - Create a new node using the value passed to the function
+    // - If there is no head property on the list, set the head and tail
+    // to be the newly created node
+    // - Otherwise set the newly created node's next property to be the current
+    // head property on the list
+    // - Set the head property on the list to be that newly created node
+    // - Increment the length of the list by 1
+    //- Return the linked list
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
@@ -87,4 +110,5 @@ list.push("shahzaib");
 list.push("coder");
 // console.log(list.pop());
 // console.log(list.shift());
+// list.unshift("The")
 console.log(list);
