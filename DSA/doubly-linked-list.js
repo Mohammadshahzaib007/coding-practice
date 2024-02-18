d8853d3f713ccf033bc77c77adc8efbc066c680d
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-02-18 18:07:24 +0530

    push done

--- DSA/doubly-linked-list.js
+++ DSA/doubly-linked-list.js
@@ -12,4 +12,21 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
