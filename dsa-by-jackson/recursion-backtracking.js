e750b246f330cadee4873518e801424b724c6767
Mohammad_shahzaib07 <shahzaib@quolam.com>
2025-01-30 21:41:35 +0530

    tower of hanoi problem

--- dsa-by-jackson/recursion-backtracking.js
+++ dsa-by-jackson/recursion-backtracking.js
@@ -70,3 +70,34 @@ function winner(n, k, approach = 3) {
 
 //----------------------------------------DAY 3----------------------------------------//
 //==========================================================================
+// Question 1: Tower of Hanoi: We have three rods and N disks.
+// The objective of the puzzle is to move the entire stack to another rod.
+// Initially, these discs are in the rod 1.
+// You need to print all the steps of discs movement so that all the discs reach the 3rd rod.
+// Also, find & return the total moves.
+
+// Note: The discs are arranged such that the top disc is numbered 1 and the bottom-most disc is numbered N.
+// Also, all the discs have different sizes and a bigger disc cannot be put on the top of a smaller disc.
+// You can only move 1 disk at a time.
+
+function toh(n, fromm, to, aux) {
+  let total = 0;
+
+  function helper(n, fromm, to, aux) {
+    if (n === 1) {
+      total++;
+      console.log("move disk " + n + " from rod " + fromm + " to rod " + to);
+      return;
+    }
+
+    helper(n - 1, fromm, aux, to);
+    total++;
+    console.log("move disk " + n + " from rod " + fromm + " to rod " + to);
+    helper(n - 1, aux, to, fromm);
+  }
+
+  helper(n, fromm, to, aux);
+  return total;
+}
+
+console.log(toh(3, 1, 3, 2));
