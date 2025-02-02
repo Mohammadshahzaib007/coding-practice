792242dc2799520a606510542cf6e50f81842676
Mohammad_shahzaib07 <shahzaib@quolam.com>
2025-02-02 13:07:58 +0530

    power sum

--- dsa-by-jackson/recursion-backtracking.js
+++ dsa-by-jackson/recursion-backtracking.js
@@ -100,4 +100,26 @@ function toh(n, fromm, to, aux) {
   return total;
 }
 
-console.log(toh(3, 1, 3, 2));
+// Question 2:
+// Power Sum: Let’s define a peculiar type of array in which each element is either an integer or another peculiar array.
+// Assume that a peculiar array is never empty. Write a function that will take a peculiar array as its input and find the
+// sum of its elements. If an array is an element in the peculiar array you have to convert it to it’s equivalent value so
+// that you can sum it with the other elements. Equivalent value of an array is the sum of its elements raised to the number
+// which represents how far nested it is. For e.g. [2,3[4,1,2]] = 2+3+ (4+1+2)^2
+
+// [1,2,[7,[3,4],2]] = 1 + 2 +( 7+(3+4)^3+2)^2
+
+function powerSum(list, depth = 1) {
+  let total = 0;
+
+  for (let num of list) {
+    if (Array.isArray(num)) {
+      total += powerSum(num, depth + 1);
+    } else {
+      total += num;
+    }
+  }
+  return Math.pow(total, depth);
+}
+
+console.log(powerSum([1, 2, [7, [3, 4], 2]]));
